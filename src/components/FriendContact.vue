f7e5380f9884e4c73cc912605b6b383bf41eb9d5
Pedro Feltrin <pedrofeltrin45@gmail.com>
2021-10-21 08:46:07 -0300

    feat: create section-08 example code

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+    <li>
+        <h2>{{ friend.name }} {{ friend.isFavorite ? '(Favorite)' : '' }}</h2>
+        <button @click="toggleDetails">
+            {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+        </button>
+        <button @click="toggleFavorite">Toggle Favorite</button>
+        <ul v-if="detailsAreVisible">
+            <li>
+                <strong>Phone:</strong>
+                {{ friend.phone }}
+            </li>
+            <li>
+                <strong>Email:</strong>
+                {{ friend.email }}
+            </li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    props: ['friend'],
+    data() {
+        return {
+            detailsAreVisible: false,
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        },
+        toggleFavorite() {
+            this.$emit('toggle-favorite', this.friend.id);
+        },
+    },
+};
+</script>
