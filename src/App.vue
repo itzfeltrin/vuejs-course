f144ca0d075d0200e69c880a17fdbff66ef81d95
Pedro Feltrin <pedrofeltrin45@gmail.com>
2021-10-27 08:59:50 -0300

    refactor: make Header, BadgeList and UserInfo local imports

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,44 @@
 <template>
-	<div>
-		<the-header></the-header>
-		<badge-list></badge-list>
-		<user-info
-			:full-name="activeUser.name"
-			:info-text="activeUser.description"
-			:role="activeUser.role"
-		></user-info>
-	</div>
+    <div>
+        <TheHeader />
+        <BadgeList />
+        <UserInfo
+            :full-name="activeUser.name"
+            :info-text="activeUser.description"
+            :role="activeUser.role"
+        />
+    </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
-	data() {
-		return {
-			activeUser: {
-				name: 'Maximilian Schwarzmüller',
-				description: 'Site owner and admin',
-				role: 'admin',
-			},
-		};
-	},
+    components: {
+        TheHeader,
+        BadgeList,
+        UserInfo,
+    },
+    data() {
+        return {
+            activeUser: {
+                name: 'Maximilian Schwarzmüller',
+                description: 'Site owner and admin',
+                role: 'admin',
+            },
+        };
+    },
 };
 </script>
 
 <style>
 html {
-	font-family: sans-serif;
+    font-family: sans-serif;
 }
 
 body {
-	margin: 0;
+    margin: 0;
 }
 </style>
