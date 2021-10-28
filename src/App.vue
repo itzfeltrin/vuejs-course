7d54ae0510928a8472c006af5464213b9fd07490
Pedro Feltrin <pedrofeltrin45@gmail.com>
2021-10-28 08:59:39 -0300

    feat: create a dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,51 @@
 <template>
     <div>
         <TheHeader />
-        <BadgeList />
+        <!-- <BadgeList />
         <UserInfo
             :full-name="activeUser.name"
             :info-text="activeUser.description"
             :role="activeUser.role"
-        />
+        /> -->
+        <button @click="changeSelectedComponent('active-goals')">
+            Active Goals
+        </button>
+        <button @click="changeSelectedComponent('manage-goals')">
+            Manage Goals
+        </button>
+        <component :is="selectedComponent"></component>
     </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
     components: {
         TheHeader,
-        BadgeList,
-        UserInfo,
+        // BadgeList,
+        // UserInfo,
+        ActiveGoals,
+        ManageGoals,
     },
     data() {
         return {
-            activeUser: {
-                name: 'Maximilian Schwarzmüller',
-                description: 'Site owner and admin',
-                role: 'admin',
-            },
+            selectedComponent: 'active-goals',
+            // activeUser: {
+            //     name: 'Maximilian Schwarzmüller',
+            //     description: 'Site owner and admin',
+            //     role: 'admin',
+            // },
         };
+    },
+    methods: {
+        changeSelectedComponent(comp) {
+            this.selectedComponent = comp;
+        },
     },
 };
 </script>
