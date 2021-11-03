3b0fdbf7a78f505c0e066063c884bf24eb16fcbd
Pedro Feltrin <pedrofeltrin45@gmail.com>
2021-11-03 08:34:09 -0300

    feat: create resources array data and LearningResource component

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul>
+        <li v-for="resource in storedResources" :key="resource.id">
+            <LearningResource :resource="resource" />
+        </li>
+    </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+    components: {
+        LearningResource,
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 1,
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org',
+                },
+                {
+                    id: 2,
+                    title: 'Google',
+                    description: 'Learn to google...',
+                    link: 'https://google.com',
+                },
+            ],
+        };
+    },
+};
+</script>
---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header>
+            <h3>{{ resource.title }}</h3>
+            <button>
+                <span>Delete</span>
+            </button>
+        </header>
+        <p>{{ resource.description }}</p>
+        <nav>
+            <a :href="resource.link" target="_blank">View Resource</a>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['resource'],
+};
+</script>
