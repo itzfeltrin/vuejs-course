408f0a02db4151dd2e1ff425dee17c0eb9bf61be
Pedro Feltrin <pedrofeltrin45@gmail.com>
2021-11-10 08:02:08 -0300

    feat: add more config files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ["plugin:vue/essential", "standard"],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	plugins: ["vue"],
+	rules: {
+		indent: ["error", 4],
+		semi: "off",
+		"comma-dangle": "off",
+		"space-before-function-paren": "off",
+		"vue/no-multiple-template-root": "off",
+	},
+};
