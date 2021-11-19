f7dce8325ec97f8c925ed132c98ca22d0ff0f789
Pedro Feltrin <pedrofeltrin45@gmail.com>
2021-11-19 08:27:15 -0300

    feat: setup router

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachList from '../pages/coach/CoachList.vue';
+import CoachDetails from '../pages/coach/CoachDetails.vue';
+import CoachContact from '../pages/coach/CoachContact.vue';
+import CoachRegister from '../pages/coach/CoachRegister.vue';
+import RequestsReceived from '../pages/requests/RequestsReceived.vue';
+import NotFound from '../pages/NotFound.vue';
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            redirect: '/coaches',
+        },
+        {
+            path: '/coaches',
+            component: CoachList,
+        },
+        {
+            path: '/coaches/:id',
+            component: CoachDetails,
+            children: [
+                {
+                    path: '/contact',
+                    component: CoachContact,
+                },
+            ],
+        },
+        {
+            path: '/register',
+            component: CoachRegister,
+        },
+        {
+            path: '/requests',
+            component: RequestsReceived,
+        },
+        {
+            path: '/:notFound(*)',
+            component: NotFound,
+        },
+    ],
+});
