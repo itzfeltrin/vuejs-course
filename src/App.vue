448d2f76d5cd7358368a377a1c2d701f2a5f7346
Pedro Feltrin <pedrofeltrin45@gmail.com>
2021-12-03 08:45:02 -0300

    feat: finish assignement with Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,42 @@
 <template>
-	<h2>My Course Goal</h2>
-	<!-- Task 1: Output your main course goal with help of the composition API -->
-	<!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-	<h3>OUTPUT COURSE GOAL</h3>
-	<!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-	<button>Toggle Goal</button>
-	<!-- Task 3: Manage data in three ways -->
-	<!-- => Separate refs -->
-	<!-- => Ref Object -->
-	<!-- => Reactive Object -->
-	<!-- Task 4: Also solve the assignment with the Options API -->
+    <h2>My Course Goal</h2>
+    <!-- Task 1: Output your main course goal with help of the composition API -->
+    <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+    <h3 v-if="goal.isVisible">{{ goal.text }}</h3>
+    <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+    <button @click="toggleGoal">Toggle Goal</button>
+    <!-- Task 3: Manage data in three ways -->
+    <!-- => Separate refs -->
+    <!-- => Ref Object -->
+    <!-- => Reactive Object -->
+    <!-- Task 4: Also solve the assignment with the Options API -->
 </template>
 
 <script>
-export default {};
+import { reactive } from 'vue';
+
+export default {
+    setup() {
+        const goal = reactive({
+            text: 'Learn enough about Vue.js to build a real world application.',
+            isVisible: true,
+        });
+
+        const toggleGoal = () => {
+            goal.isVisible = !goal.isVisible;
+        };
+
+        return { goal, toggleGoal };
+    },
+};
 </script>
 
 <style>
 html {
-	font-family: sans-serif;
+    font-family: sans-serif;
 }
 body {
-	margin: 3rem;
-	text-align: center;
+    margin: 3rem;
+    text-align: center;
 }
 </style>
